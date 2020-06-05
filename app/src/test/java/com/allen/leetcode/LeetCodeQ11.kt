d44bd99358552e92460a506a218bf0226b75d21a
allen843163 <liyuzong0316@gmail.com>
2020-06-05 10:47:50 +0800

    hi

--- app/src/test/java/com/allen/leetcode/LeetCodeQ11.kt
+++ app/src/test/java/com/allen/leetcode/LeetCodeQ11.kt
@@ -13,55 +13,70 @@ class LeetCodeQ11 {
 
         var map = HashMap<Int, Int>()
 
-        var searchLength =  (height.size / 2) + (height.size % 2)
-
         var nowLeftBoard = 0;
 
         var nowRightMaxIndex = height.size - 1
 
         var nowRightBoard = 0
 
-        for(i in 0 .. searchLength) {
+        for(i in height.indices) {
             println("i = ".plus(i))
 
-            if(map.containsKey(i)) {
+            if(map.containsKey(i) || height[i] <= nowLeftBoard) {
+                println("continue1")
+
                 continue
             }
-            else if(height[i] <= nowLeftBoard) {
-                continue
+            else if(i >= nowRightMaxIndex) {
+                break
             }
+            else {
 
-            for(z in nowRightMaxIndex downTo i) {
-                println("z = ".plus(z))
+                if(height[nowRightMaxIndex] >= height[i]) {
+                    var max = height[i] * (nowRightMaxIndex - i)
 
-                if(map.containsKey(z)) {
-                    continue
-                }
-                else if(height[z] <= nowRightBoard) {
-                    continue
-                }
-
-                if(height[z] >= height[i]) {
-                    var max = height[i] * (z - i)
-
-                    println("max = ".plus(max))
+                    println("max1 = ".plus(max))
 
                     map[i] = max
 
                     nowLeftBoard = maxOf(height[i], nowLeftBoard)
 
-                    nowRightMaxIndex = z
-
-                    break
+                    continue
                 }
                 else {
-                    var max = height[z] * (z - i)
+                    while (nowRightMaxIndex > i) {
+                        println("nowRightMaxIndex".plus(nowRightMaxIndex))
 
-                    println("max = ".plus(max))
+                        if(height[nowRightMaxIndex] >= height[i]) {
+                            var max = height[i] * (nowRightMaxIndex - i)
 
-                    map[z] = max
+                            println("max1 = ".plus(max))
 
-                    nowRightBoard = maxOf(height[z], nowRightBoard)
+                            map[i] = max
+
+                            nowLeftBoard = maxOf(height[i], nowLeftBoard)
+
+                            break
+                        }
+
+                        if(map.containsKey(nowRightMaxIndex) || height[nowRightMaxIndex] < nowRightBoard) {
+                            nowRightMaxIndex --
+
+                            continue
+                        }
+
+                        var max = height[nowRightMaxIndex] * (nowRightMaxIndex - i)
+
+                        println("max2 = ".plus(max))
+
+                        map[nowRightMaxIndex] = max
+
+                        nowRightBoard = maxOf(height[nowRightMaxIndex], nowRightBoard)
+
+                        println("nowRightBoard".plus(nowRightBoard))
+
+                        nowRightMaxIndex --
+                    }
                 }
             }
         }
@@ -74,9 +89,12 @@ class LeetCodeQ11 {
     @ExperimentalStdlibApi
     @Test
     fun addition_isCorrect() {
-//        assertEquals(49, ContainerWithMostWater_MySelf(intArrayOf(1,8,6,2,5,4,8,3,7)))
+        assertEquals(49, ContainerWithMostWater_MySelf(intArrayOf(1,8,6,2,5,4,8,3,7)))
         assertEquals(24, ContainerWithMostWater_MySelf(intArrayOf(1,2,3,4,5,25,24,3,4)))
         assertEquals(72, ContainerWithMostWater_MySelf(intArrayOf(9,6,14,11,2,2,4,9,3,8)))
+        assertEquals(112, ContainerWithMostWater_MySelf(intArrayOf(14,0,12,3,8,3,13,5,14,8)))
+        assertEquals(55, ContainerWithMostWater_MySelf(intArrayOf(4,4,2,11,0,11,5,11,13,8)))
+        assertEquals(42, ContainerWithMostWater_MySelf(intArrayOf(4,6,4,4,6,2,6,7,11,2)))
 
     }
 }
